3864e36be7977af9c5642a172cccc901112f9d88
Torben Lange <tlange@physnet.uni-hamburg.de>
2015-06-09 10:39:14 +0200

    changed the likelihood constraint, the constraint now uses TH1D* instead of TF1*

--- include/HHFitConstraintLikelihood.h
+++ include/HHFitConstraintLikelihood.h
@@ -8,19 +8,22 @@
 #include "HHFitConstraint.h"
 #include "TF1.h"
 #include "HHFitObject.h"
+#include "TH1D.h"
 
 
 namespace HHKinFit2{
 class HHFitConstraintLikelihood : public HHFitConstraint {
  public:
   HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TF1* likelihood1, TF1* likelihood2 );
-
+  HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TH1D* likelihoodhisto1, TH1D* likelihoodhisto2 );
   Double_t getChi2() const;
   double getLikelihood() const;
 
  private:
   TF1* const m_likelihood1;
   TF1* const m_likelihood2;
+  TH1D* const m_likelihoodhisto1;
+  TH1D* const m_likelihoodhisto2;
   HHFitObject* m_object2;
 
 };
--- src/HHFitConstraintLikelihood.cpp
+++ src/HHFitConstraintLikelihood.cpp
@@ -11,14 +11,33 @@ HHKinFit2::HHFitConstraintLikelihood::HHFitConstraintLikelihood(HHFitObject* obj
 
 }
 
-Double_t 
+HHKinFit2::HHFitConstraintLikelihood::HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TH1D* likelihoodhisto1, TH1D* likelihoodhisto2 )
+  : HHFitConstraint(object1),
+    m_likelihoodhisto1(likelihoodhisto1),
+    m_likelihoodhisto2(likelihoodhisto2),
+    m_object2(object2){
+
+}
+
+/*Double_t
 HHKinFit2::HHFitConstraintLikelihood::getChi2() const{
 	double chi2=-2*log(m_likelihood1->Eval(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E()))-2*log(m_likelihood2->Eval(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E()));
   return(chi2);
-}
+}*
+
+*/
+
+Double_t 
+HHKinFit2::HHFitConstraintLikelihood::getChi2() const{
+  double chi2=-2*log(m_likelihoodhisto1->GetBinContent(m_likelihoodhisto1->FindBin(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E())))-2*log(m_likelihoodhisto2->GetBinContent(m_likelihoodhisto2->FindBin(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E())));
+  return(chi2);
+
+/*double
+HHKinFit2::HHFitConstraintLikelihood::getLikelihood() const{
+  return(m_likelihood1->Eval(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E())*m_likelihood2->Eval(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E()));
+}*/
 
 double
 HHKinFit2::HHFitConstraintLikelihood::getLikelihood() const{
-  return(m_likelihood1->Eval(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E())*m_likelihood2->Eval(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E()));
+  return(m_likelihoodhisto1->GetBinContent(m_likelihoodhisto1->FindBin(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E()))*m_likelihoodhisto2->GetBinContent(m_likelihoodhisto2->FindBin(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E())));
 }
-
