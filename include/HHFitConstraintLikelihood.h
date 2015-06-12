ebf4426ddbfdafc97135cbe8fb177b1a7d0ab162
Torben Lange <tlange@physnet.uni-hamburg.de>
2015-06-12 13:38:09 +0200

    HHFitConstraintLikelihood now can use two TF1* or two TH1D* or one TH2D* to get the pdf-information

--- include/HHFitConstraintLikelihood.h
+++ include/HHFitConstraintLikelihood.h
@@ -9,6 +9,7 @@
 #include "TF1.h"
 #include "HHFitObject.h"
 #include "TH1D.h"
+#include "TH2D.h"
 
 
 namespace HHKinFit2{
@@ -16,6 +17,7 @@ class HHFitConstraintLikelihood : public HHFitConstraint {
  public:
   HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TF1* likelihood1, TF1* likelihood2 );
   HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TH1D* likelihoodhisto1, TH1D* likelihoodhisto2 );
+  HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TH2D* likelihoodhisto);
   Double_t getChi2() const;
   double getLikelihood() const;
 
@@ -24,7 +26,9 @@ class HHFitConstraintLikelihood : public HHFitConstraint {
   TF1* const m_likelihood2;
   TH1D* const m_likelihoodhisto1;
   TH1D* const m_likelihoodhisto2;
+  TH2D* const m_likelihoodhisto;
   HHFitObject* m_object2;
+  int mode;
 
 };
 }
--- src/HHFitConstraintLikelihood.cpp
+++ src/HHFitConstraintLikelihood.cpp
@@ -7,18 +7,35 @@ HHKinFit2::HHFitConstraintLikelihood::HHFitConstraintLikelihood(HHFitObject* obj
   : HHFitConstraint(object1),
     m_likelihood1(likelihood1),
     m_likelihood2(likelihood2),
+    m_likelihoodhisto1(),
+    m_likelihoodhisto2(),
+    m_likelihoodhisto(),
+    mode(1),
     m_object2(object2){
 
 }
 
 HHKinFit2::HHFitConstraintLikelihood::HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TH1D* likelihoodhisto1, TH1D* likelihoodhisto2 )
   : HHFitConstraint(object1),
+    m_likelihood1(),
+    m_likelihood2(),
     m_likelihoodhisto1(likelihoodhisto1),
     m_likelihoodhisto2(likelihoodhisto2),
+    m_likelihoodhisto(),
+    mode(2),
     m_object2(object2){
-
 }
 
+HHKinFit2::HHFitConstraintLikelihood::HHFitConstraintLikelihood(HHFitObject* object1,HHFitObject* object2, TH2D* likelihoodhisto)
+  : HHFitConstraint(object1),
+    m_likelihood1(),
+    m_likelihood2(),
+    m_likelihoodhisto1(),
+    m_likelihoodhisto2(),
+    m_likelihoodhisto(likelihoodhisto),
+    mode(3),
+    m_object2(object2){
+}
 /*Double_t
 HHKinFit2::HHFitConstraintLikelihood::getChi2() const{
 	double chi2=-2*log(m_likelihood1->Eval(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E()))-2*log(m_likelihood2->Eval(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E()));
@@ -29,9 +46,19 @@ HHKinFit2::HHFitConstraintLikelihood::getChi2() const{
 
 Double_t 
 HHKinFit2::HHFitConstraintLikelihood::getChi2() const{
+	if(mode==1){
+	double chi2=-2*log(m_likelihood1->Eval(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E()))-2*log(m_likelihood2->Eval(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E()));
+    return(chi2);
+	}
+	if(mode==2){
   double chi2=-2*log(m_likelihoodhisto1->GetBinContent(m_likelihoodhisto1->FindBin(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E())))-2*log(m_likelihoodhisto2->GetBinContent(m_likelihoodhisto2->FindBin(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E())));
   return(chi2);
-
+	}
+	if(mode==3){
+		double chi2=-2*log(m_likelihoodhisto->GetBinContent(m_likelihoodhisto->FindBin(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E(),m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E() )));
+		  return(chi2);
+	}
+}
 /*double
 HHKinFit2::HHFitConstraintLikelihood::getLikelihood() const{
   return(m_likelihood1->Eval(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E())*m_likelihood2->Eval(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E()));
@@ -39,5 +66,14 @@ HHKinFit2::HHFitConstraintLikelihood::getLikelihood() const{
 
 double
 HHKinFit2::HHFitConstraintLikelihood::getLikelihood() const{
-  return(m_likelihoodhisto1->GetBinContent(m_likelihoodhisto1->FindBin(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E()))*m_likelihoodhisto2->GetBinContent(m_likelihoodhisto2->FindBin(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E())));
+	if(mode==1){
+		return(m_likelihood1->Eval(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E())*m_likelihood2->Eval(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E()));
+	}
+	if(mode==2){
+        return(m_likelihoodhisto1->GetBinContent(m_likelihoodhisto1->FindBin(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E()))*m_likelihoodhisto2->GetBinContent(m_likelihoodhisto2->FindBin(m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E())));
+	}
+	if(mode==3){
+		return(m_likelihoodhisto->GetBinContent(m_likelihoodhisto->FindBin(m_fitobject->getInitial4Vector().E()/m_fitobject->getFit4Vector().E(),m_object2->getInitial4Vector().E()/m_object2->getFit4Vector().E() )));
+	}
+
 }
