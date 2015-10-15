0db30e6333a74e30f36768903cc493cffdcaab72
Malte Hoffmann <malte.hoffmann@desy.de>
2015-10-15 11:26:25 +0200

    renamed include/ to interface/

--- compile.sh
+++ compile.sh
@@ -11,14 +11,14 @@
      rm -f libHHKinFit2.so
     
      echo "creating shared library"
-     g++ -Wall -fPIC -shared src/*.cpp `root-config --cflags --glibs` -I ./include -o libHHKinFit2.so
+     g++ -Wall -fPIC -shared src/*.cpp `root-config --cflags --glibs` -I ./interface -o libHHKinFit2.so
      echo "creating executable"
 #     #g++ examples/main.C `root-config --cflags --glibs` -I $KINFIT2PATH/include -L $KINFIT2PATH -lHHKinFit2  -o runHHKinFit
 #     #g++ -std=c++11 examples/compareKinFits.C `root-config --cflags --glibs` -I $KINFIT2PATH/include -I $KINFIT1PATH/interface -L $KINFIT2PATH -lHHKinFit2 -L $KINFIT1PATH -lHHKinFit1 -o compareKinFits
 #     g++ -std=c++11 examples/compare2DKinFits.C `root-config --cflags --glibs` -I $KINFIT2PATH/include -I $KINFIT1PATH/interface -L $KINFIT2PATH -lHHKinFit2 -L $KINFIT1PATH -lHHKinFit1 -o compare2DKinFits
 #     #g++ -std=c++11 examples/gensimHH.C `root-config --cflags --glibs` -I $KINFIT2PATH/include -I $KINFIT1PATH/interface -L $KINFIT2PATH -lHHKinFit2 -o gensimHH
 #     #g++ -std=c++11 examples/example.C `root-config --cflags --glibs` -I $KINFIT2PATH/include -I $KINFIT1PATH/interface -L $KINFIT2PATH -lHHKinFit2 -o example
-    g++ examples/fitSingleEvent2DKinFit.C `root-config --cflags --glibs` -I $KINFIT2PATH/include -L $KINFIT2PATH -lHHKinFit2 -o fitSingleEvent2DKinFit
-    g++ examples/fitSingleEvent1DKinFit.C `root-config --cflags --glibs` -I $KINFIT2PATH/include -L $KINFIT2PATH -lHHKinFit2 -o fitSingleEvent1DKinFit
+    g++ examples/fitSingleEvent2DKinFit.C `root-config --cflags --glibs` -I $KINFIT2PATH/interface -L $KINFIT2PATH -lHHKinFit2 -o fitSingleEvent2DKinFit
+    g++ examples/fitSingleEvent1DKinFit.C `root-config --cflags --glibs` -I $KINFIT2PATH/interface -L $KINFIT2PATH -lHHKinFit2 -o fitSingleEvent1DKinFit
  fi
 
